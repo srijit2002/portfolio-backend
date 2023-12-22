9122b16a270faa630c7489e66d304749bb69fbde
Srijit <srijitm906@gmail.com>
2023-12-22 21:53:50 +0530

    fix: Handle cors requests

--- main.go
+++ main.go
@@ -6,7 +6,25 @@ import (
 	"portfolio-backend/routers"
 )
 
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*") // adjust this to suit your needs
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Check if it's a preflight request and handle it
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Pass down the request to the next handler (or middleware)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	r:=routers.Router()
-	log.Fatal(http.ListenAndServe(":8000",r))
+	router := routers.Router()
+	log.Fatal(http.ListenAndServe(":8000", enableCORS(router)))
 }
